92f1f44d0144843edbd1c0c8412cd9b20768a5c2
Josh <39458297+jgagan95@users.noreply.github.com>
2020-12-20 12:36:18 -0600

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,17 @@
-import os, time
+import schedule, time, os, datetime
 
-# os.chdir('C:\\path')
-# os.mkdir('Test')
+#based on the date, create a folder in a specific directory for each day in month
+def job():
+  #obtain the date
+  full_date = datetime.date.today() #prints to year-month-day format
+  todays_date = f"{full_date}"[5:] #will give me just month-day format of date; is a string and not object anymore?
+  #go into an already created folder and make a new directory based on the day
+  os.chdir('C:\\Users\\Josh Agan\\Desktop\\Test')
+  os.mkdir(todays_date)
 
-print(time.time())
+#creates the folder in 'Test' every day at 12:02 AM
+schedule.every().day.at('00:02').do(job)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
